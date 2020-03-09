f625b145b2c7072c01dc1b94a2e7bc1ead148a2f
sohadQtaitat <qtaitat3@gmail.com>
2020-03-09 06:37:16 +0200

    setup file ... need more work

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Status from './component/status.js';
+import Update from '../src/components/catagore-counter.js';
+export default props =>{
+  return(
+    <>
+      <Update/>
+      <Status/>
+    </>
+  );
+};
---
+++ src/components/catagore-counter.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { render, reset } from '../store/action.js/index.js';
+
+
